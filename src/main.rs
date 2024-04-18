6025346b92f26ed3451d499c76e7183325d2f6d5
Jon Downs <jon@yann.downsnet>
2024-04-18 09:05:58 -0700

    ready to try

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,30 @@ pub mod config;
 pub mod posts;
 use crate::config::*;
 use crate::posts::*;
-
 use axum::{
     extract::Host,
     handler::HandlerWithoutStateExt,
-    http::{StatusCode, Uri},
+    http::{Request, StatusCode, Uri},
     response::{IntoResponse, Redirect},
     routing::get,
     BoxError, Router,
 };
-use axum_server::tls_rustls::RustlsConfig;
+use hyper::body::Incoming;
+
+use futures_util::pin_mut;
+use hyper_util::rt::{TokioExecutor, TokioIo};
+use openssl::ssl::{Ssl, SslAcceptor, SslFiletype, SslMethod};
 use sqlx::postgres::PgPoolOptions;
 use std::env;
+use std::pin::Pin;
 use std::sync::Arc;
 use std::time::Duration;
 use std::{net::SocketAddr, path::PathBuf};
+use tokio::net::TcpListener;
+use tokio_openssl::SslStream;
+use tower::Service;
 use tower_http::services::ServeDir;
+use tracing::{error, info, warn};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[allow(dead_code)]
@@ -39,18 +47,31 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
+    let mut tls_builder = SslAcceptor::mozilla_modern_v5(SslMethod::tls()).unwrap();
+
+    tls_builder
+        .set_certificate_file(
+            PathBuf::from(&cfg.pem_dir).join("cert.pem"),
+            SslFiletype::PEM,
+        )
+        .unwrap();
+
+    tls_builder
+        .set_private_key_file(
+            PathBuf::from(&cfg.pem_dir).join("privkey.pem"),
+            SslFiletype::PEM,
+        )
+        .unwrap();
+
+    tls_builder.check_private_key().unwrap();
+
+    let tls_acceptor = tls_builder.build();
+
     let ports = Ports {
         http: cfg.port_http,
         https: cfg.port_https,
     };
     tokio::spawn(redirect_http_to_https(ports));
-
-    let tlscfg = RustlsConfig::from_pem_file(
-        PathBuf::from(&cfg.pem_dir).join("cert.pem"),
-        PathBuf::from(&cfg.pem_dir).join("privkey.pem"),
-    )
-    .await
-    .unwrap();
 
     let db_connection_str = std::env::var("DATABASE_URL").unwrap_or_else(|_| cfg.cnxn_str);
 
@@ -89,12 +110,44 @@ async fn main() {
     let app = app.fallback(handler_404);
 
     // run it with hyper
-    let listener = SocketAddr::from(([0, 0, 0, 0], ports.https));
-    tracing::debug!("listening on {}", listener);
-    axum_server::bind_rustls(listener, tlscfg)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    let bind = "[::1]:3000";
+    let tcp_listener = TcpListener::bind(bind).await.unwrap();
+    info!("HTTPS server listening on {bind}. To contact curl -k https://localhost:3000");
+    pin_mut!(tcp_listener);
+
+    loop {
+        let tower_service = app.clone();
+        let tls_acceptor = tls_acceptor.clone();
+
+        // Wait for new tcp connection
+        let (cnx, addr) = tcp_listener.accept().await.unwrap();
+
+        tokio::spawn(async move {
+            let ssl = Ssl::new(tls_acceptor.context()).unwrap();
+            let mut tls_stream = SslStream::new(ssl, cnx).unwrap();
+            if let Err(err) = SslStream::accept(Pin::new(&mut tls_stream)).await {
+                error!(
+                    "error during tls handshake connection from {}: {}",
+                    addr, err
+                );
+                return;
+            }
+
+            let stream = TokioIo::new(tls_stream);
+
+            let hyper_service = hyper::service::service_fn(move |request: Request<Incoming>| {
+                tower_service.clone().call(request)
+            });
+
+            let ret = hyper_util::server::conn::auto::Builder::new(TokioExecutor::new())
+                .serve_connection_with_upgrades(stream, hyper_service)
+                .await;
+
+            if let Err(err) = ret {
+                warn!("error serving connection from {}: {}", addr, err);
+            }
+        });
+    }
 }
 
 async fn handler_404() -> impl IntoResponse {
@@ -131,10 +184,11 @@ async fn redirect_http_to_https(ports: Ports) {
         }
     };
 
-    let addr = SocketAddr::from(([0, 0, 0, 0], ports.http));
-    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
-    tracing::debug!("listening on {}", listener.local_addr().unwrap());
-    axum::serve(listener, redirect.into_make_service())
+    // run it with hyper
+    let bind = "[::1]:7878";
+    let tcp_listener = TcpListener::bind(bind).await.unwrap();
+    tracing::debug!("listening on {}", tcp_listener.local_addr().unwrap());
+    axum::serve(tcp_listener, redirect.into_make_service())
         .await
         .unwrap();
 }
