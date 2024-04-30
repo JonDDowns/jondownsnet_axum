d837a1bb7916688e51944d99c6b12738cee0f850
Jon Downs <jon@jondowns.net>
2024-04-30 15:37:30 -0700

    Adding post number 6

--- sql/new_post.sql
+++ sql/new_post.sql
@@ -1,8 +1,10 @@
-\set post `cat ~/projects/jondownsnet_axum/raw_html/rust_r_trigrams.html`
-INSERT INTO posts (post_date, post_title, post_body, post_summary)
+\set post `cat ~/projects/jondownsnet_axum/rust/htmlpages/raw/this_one_mistake.html`
+INSERT INTO posts (post_date, post_title, post_body, post_summary, post_thumbnail, post_thumbnail_alt)
 VALUES (
-	'2024-04-11',
-	'Need Performance? Use Rust!', 
+	'2024-04-26',
+	'This One Mistake Slows Your R Code Down by 200x', 
 	:'post', 
-	'Comparing cosine string distance performance in R and Rust.'
+	'You might be surprised at how the for loop does.',
+	'../assets/images/shuaib-khokhar-unsplash.webp',
+	'Speedometer image. Courtesy of Shuaib Khokhar via unsplash.'
 );
--- sql/update_post.sql
+++ sql/update_post.sql
@@ -1,3 +1,6 @@
-\set post `cat ./htmlpages/minified/RUserPythonGuide.html`
-UPDATE posts SET post_body = :'post' WHERE post_id = 4;
-
+\set post `cat ./htmlpages/minified/this_one_mistake.html`
+UPDATE posts SET 
+	post_body = :'post'
+	, post_thumbnail = '../assets/images/shuaib-khokhar-unsplash.webp'
+	, post_thumbnail_alt = 'Speedometer image. Courtesy of Shuaib Khokhar via unsplash.'
+WHERE post_id = 6;
